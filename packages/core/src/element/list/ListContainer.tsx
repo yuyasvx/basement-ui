d425f1bd8926dd49ff75a66b6b748cf134e5a21b
yuyasvx <yuyasvx@gmail.com>
2023-09-19 01:12:01 +0900

    ドキュメント削除・微修正

--- packages/core/src/element/list/ListContainer.tsx
+++ packages/core/src/element/list/ListContainer.tsx
@@ -15,11 +15,9 @@ const NAME = 'bm-e-list-container';
 const HEADER_NAME = `${NAME}__header`;
 const FOOTER_NAME = `${NAME}__footer`;
 
-export type ListContainerProps = BaseComponentProps<HTMLDivElement> &
-  ListContainerDetailedProps &
-  MouseEvents<HTMLDivElement>;
+export type ListContainerProps = BaseComponentProps & ListContainerDetailedProps & MouseEvents<HTMLDivElement>;
 export const ListContainer: FC<PropsWithChildren<ListContainerProps>> = props => {
-  const p = getBaseComponentProps<HTMLDivElement, ListContainerProps>(props);
+  const p = getBaseComponentProps(props);
   const me = getMouseEventHandler(props);
   const classNames = useMemo(
     () => clsx(NAME, RootStyle.BASE, RootStyle.TEXT_BASE, { '-sticky': props.stickyHeader }, props.className),
